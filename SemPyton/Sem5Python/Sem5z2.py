b98be734d8ea994b4288b7575ef6af2fc868d3c6
Lyamina Ekaterina <katjamina@rambler.ru>
2022-12-03 16:56:39 +0300

    Задача 2

--- SemPyton/Sem5Python/Sem5z2.py
+++ SemPyton/Sem5Python/Sem5z2.py
@@ -4,6 +4,50 @@
 #b) Подумайте как наделить бота ""интеллектом""
 
 # вариант человек против человека:
+#from random import randint
+
+#def input_dat(name):
+   # x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+   # while x < 1 or x > 28:
+    #    x = int(input(f"{name}, введите корректное количество конфет: "))
+   # return x
+
+
+#def p_print(name, k, counter, value):
+#    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+#player1 = input("Введите имя первого игрока: ")
+#player2 = input("Введите имя второго игрока: ")
+#value = int(input("Введите количество конфет на столе: "))
+#flag = randint(0,2) # флаг очередности
+#if flag:
+ #   print(f"Первый ходит {player1}")
+#else:
+ #   print(f"Первый ходит {player2}")
+
+#counter1 = 0 
+#counter2 = 0
+
+#while value > 28:
+    #if flag:
+       # k = input_dat(player1)
+        #counter1 += k
+        #value -= k
+        #flag = False
+        #p_print(player1, k, counter1, value)
+    #else:
+        #k = input_dat(player2)
+        #counter2 += k
+        #value -= k
+        #flag = True
+        #p_print(player2, k, counter2, value)
+
+#if flag:
+    #print(f"Выиграл {player1}")
+#else:
+    #print(f"Выиграл {player2}")
+
+    # вариант человек против бота:
 from random import randint
 
 def input_dat(name):
@@ -17,7 +61,7 @@ def p_print(name, k, counter, value):
     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
 player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
+player2 = "Bot"
 value = int(input("Введите количество конфет на столе: "))
 flag = randint(0,2) # флаг очередности
 if flag:
@@ -25,10 +69,10 @@ if flag:
 else:
     print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
+#counter1 = 0 
+#counter2 = 0
 
-while value > 28:
+#while value > 28:
     if flag:
         k = input_dat(player1)
         counter1 += k
@@ -36,7 +80,7 @@ while value > 28:
         flag = False
         p_print(player1, k, counter1, value)
     else:
-        k = input_dat(player2)
+        k = randint(1,29)
         counter2 += k
         value -= k
         flag = True
@@ -46,4 +90,3 @@ if flag:
     print(f"Выиграл {player1}")
 else:
     print(f"Выиграл {player2}")
-    
